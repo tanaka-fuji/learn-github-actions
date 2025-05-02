797e6497dff26e28208dece6e06a7f316439493f
Kiyoto Tanaka <tanaka.kiyoto56@gmail.com>
2025-05-02 19:24:26 +0900

    baseURLの追加

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
@@ -15,4 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: '/learn-github-actions/'
 })
